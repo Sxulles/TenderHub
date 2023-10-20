ac4f94924b5d4185444191a835015aad82e6e0db
Sxulles <seres1322@gmail.com>
2023-10-20 13:21:56 +0200

    Add routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import LoginPage from './pages/LoginPage';
 import AdvertisementPage from './pages/AdvertisementPage';
 import NavMenu from './components/NavMenu';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+
+  const [user, setUser] = useState("");
+
+  const usernameSetter = (username) => {
+    setUser(prev => prev = username)
+  }
+
   return (
-    <>
-    <NavMenu/>
-    <AdvertisementPage/>
-    </>
+    <Router>
+      <div>
+        <NavMenu />
+        <Routes>
+          <Route path="/login" element={<LoginPage usernameSetter={usernameSetter} />} />
+          <Route path="/advertisements" element={<AdvertisementPage user={user} />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
